9dd6d5303716dfe268737017a3c69e4fdd27d747
SistemasGiroAgency <sistemas.giroagency@gmail.com>
2024-03-04 15:12:45 -0600

    Ultimo ahora si

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -63,9 +63,6 @@
   .palabrasmain h2{
     color: #141722;
     font-size: 8vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
   }
 
   .palabrasmain p{
@@ -273,7 +270,7 @@
             <div class="container" style="max-width: fit-content;">
               <div class="row">
                 <div class="col-md-10 offset-md-1 palabrasmain tx-cn">
-                  <h2 class="roboto-bold text-uppercase mt-5 pt-5 txt-fx slide-up responsiveh2"><div>Elegancia Duradera</div></h2>
+                  <div class="d-flex align-items-center justify-content-center mt-5 pt-5"><h2 class="roboto-bold text-uppercase txt-fx slide-up responsiveh2">Elegancia Duradera</h2></div>
                   <p class="caption responsivep">Combina tendencias contemporáneas con la resistencia del tiempo, asegurando que cada pieza sea tan atemporal como sofisticada.</p>
                   <a href="/catalogo" class="btn btn-mio text-uppercase rounded-3 px-4 py-3 mt-3">Adquierela hoy</a>
                 </div>
@@ -287,7 +284,7 @@
             <div class="container">
               <div class="row">
                 <div class="col-md-10 offset-md-1 palabrasmain">
-                  <h2 class="roboto-bold text-uppercase mt-5 pt-5 txt-fx slide-up responsiveh2"><div>Confort en cada detalle</div></h2>
+                  <div class="d-flex align-items-center justify-content-center mt-5 pt-5"><h2 class="roboto-bold text-uppercase txt-fx slide-up responsiveh2"><div>Confort en cada detalle</div></h2></div>
                   <p class="caption responsivep">Selección meticulosa de materiales de alta calidad, diseño enfocado en el confort, <br> para una experiencia de lujo.</p>
                   <a href="/catalogo" class="btn btn-mio text-uppercase rounded-3 px-4 py-3 mt-3">Adquierela hoy</a>
                 </div>
@@ -301,7 +298,7 @@
             <div class="container">
               <div class="row">
                 <div class="col-md-10 offset-md-1 palabrasmain">
-                  <h2 class="roboto-bold text-uppercase mt-5 pt-5 txt-fx slide-up responsiveh2"><div>Diseños Únicos</div></h2>
+                  <div class="d-flex align-items-center justify-content-center mt-5 pt-5"><h2 class="roboto-bold text-uppercase txt-fx slide-up responsiveh2"><div>Diseños Únicos</div></h2></div>
                   <p class="caption responsivep">Cada proyecto es una obra de arte, <br> diseñado para crear espacios únicos y reflejar tu personalidad.</p>
                   <a href="/catalogo" class="btn btn-mio text-uppercase rounded-3 px-4 py-3 mt-3">Adquierela hoy</a>
                 </div>
